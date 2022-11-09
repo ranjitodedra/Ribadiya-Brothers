492e13c15714698412681ad9970abbdb708d435f
RanjeetOdedra <ranjitodedra.work@example.com>
2022-11-09 09:49:08 +0530

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-// import Header from "./component/layout/Navbar/Header_no";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
@@ -8,8 +7,6 @@ import Footer from "./component/layout/Footer/Footer";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails";
 import Products from "./component/Product/Products";
-// import Search from "./component/Product/Search";
-// import LoginSignUp from "./component/User/LoginSignUp";
 import store from "./store";
 import { loadUser } from "./actions/userAction";
 import UserOptions from "./component/layout/Navbar/UserOptions_no";
@@ -19,7 +16,6 @@ import ProtectedRoute from "./component/Route/ProtectedRoute";
 import UpdateProfile from "./component/User/UpdateProfile";
 import UpdatePassword from "./component/User/UpdatePassword";
 import ForgotPassword from "./component/User/ForgotPassword";
-// import ResetPassword from "./component/User/ResetPassword";
 import Cart from "./component/Cart/Cart";
 import Shipping from "./component/Cart/Shipping";
 import ConfirmOrder from "./component/Cart/ConfirmOrder";
@@ -29,25 +25,10 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import OrderSuccess from "./component/Cart/OrderSuccess";
 import MyOrders from "./component/Order/MyOrders";
-// import OrderDetails from "./component/Order/OrderDetails";
-// import Dashboard from "./component/Admin/Dashboard.js";
-// import ProductList from "./component/Admin/ProductList.js";
-// import NewProduct from "./component/Admin/NewProduct";
-// import UpdateProduct from "./component/Admin/UpdateProduct";
-// import OrderList from "./component/Admin/OrderList";
-// import ProcessOrder from "./component/Admin/ProcessOrder";
-// import UsersList from "./component/Admin/UsersList";
-// import UpdateUser from "./component/Admin/UpdateUser";
-// import ProductReviews from "./component/Admin/ProductReviews";
-// import Contact from "./component/layout/Contact/Contact";
-import About from "./component/layout/About/About";
-// import NotFound from "./component/layout/Not Found/NotFound";
+import OrderDetails from "./component/Order/OrderDetails";
+import NotFound from "./component/layout/Not Found/NotFound";
 import Navbar from "./component/layout/Navbar/Navbar"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Saree from "./component/Catagories/Saree";
-import ChaniyaCholi from "./component/Catagories/ChaniyaCholi";
-import Gowns from "./component/Catagories/Gowns";
-// import Gown from "./component/Catagories/Gowns";
 import Login from "./component/User/Login/Login"
 import SignUp from "./component/User/SignUp/SignUp"
 
@@ -87,134 +68,32 @@ function App() {
           <ProtectedRoute exact path="/process/payment" component={Payment} />
         </Elements>
       )}
-
-      
       <Switch>
-      
         <Route exact path="/product/:id" component={ProductDetails} />
         <Route exact path="/products" component={Products} />
         <Route path="/products/:keyword" component={Products} />
-
-
-
-
-        {/* <Route exact path="/search" component={Search} />
-
-        <Route exact path="/contact" component={Contact} />
-
-        <Route exact path="/about" component={About} />
-
-        <ProtectedRoute exact path="/account" component={Profile} />
-
-        <ProtectedRoute exact path="/me/update" component={UpdateProfile} />
-
-        <ProtectedRoute
-          exact
-          path="/password/update"
-          component={UpdatePassword}
-        />
-
-        
-        */}
-
-  
-
         <Route exact path="/" component={Home} />
-        <Route exact path="/saree" component={Saree} />
-        <Route exact path="/chaniya-choli" component={ChaniyaCholi} />
-        <Route exact path="/gowns" component={Gowns} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/SignUp" component={SignUp} />
-        <Route exact path="/shipping" component={Shipping} />
-        <Route exact path="/orders" component={MyOrders} />
+        <ProtectedRoute exact path="/shipping" component={Shipping} />
+        <ProtectedRoute exact path="/orders" component={MyOrders} />
         <Route exact path="/cart" component={Cart} />
-        <Route exact path="/account" component={Profile} />
-        <Route exact path="/order/confirm" component={ConfirmOrder} />
+        <ProtectedRoute exact path="/account" component={Profile} />
+        <ProtectedRoute exact path="/order/confirm" component={ConfirmOrder} />
+        <ProtectedRoute exact path="/order/:id" component={OrderDetails} />
         <Route exact path="/process/payment" component={Payment} />
-        <Route exact path="/process/success" component={OrderSuccess} />
+        <ProtectedRoute exact path="/process/success" component={OrderSuccess} />
         <Route exact path="/me/update" component={UpdateProfile} />
         <Route exact path="/password/update" component={UpdatePassword} />
         <Route exact path="/password/forgot" component={ForgotPassword} /> 
-
-        {/* <ProtectedRoute exact path="/shipping" component={Shipping} /> */}
-
-        {/* <ProtectedRoute exact path="/success" component={OrderSuccess} /> */}
-
-        {/* <ProtectedRoute exact path="/orders" component={MyOrders} /> */}
-
-        {/* <ProtectedRoute exact path="/order/confirm" component={ConfirmOrder} /> */}
-
-        {/* <ProtectedRoute exact path="/order/:id" component={OrderDetails} /> */}
-
-        {/* <ProtectedRoute
-          isAdmin={true}
-          exact
-          path="/admin/dashboard"
-          component={Dashboard}
-        />
-        <ProtectedRoute
-          exact
-          path="/admin/products"
-          isAdmin={true}
-          component={ProductList}
-        />
-        <ProtectedRoute
-          exact
-          path="/admin/product"
-          isAdmin={true}
-          component={NewProduct}
-        />
-
-        <ProtectedRoute
-          exact
-          path="/admin/product/:id"
-          isAdmin={true}
-          component={UpdateProduct}
-        />
-        <ProtectedRoute
-          exact
-          path="/admin/orders"
-          isAdmin={true}
-          component={OrderList}
-        />
-
-        <ProtectedRoute
-          exact
-          path="/admin/order/:id"
-          isAdmin={true}
-          component={ProcessOrder}
-        />
-        <ProtectedRoute
-          exact
-          path="/admin/users"
-          isAdmin={true}
-          component={UsersList}
-        />
-
-        <ProtectedRoute
-          exact
-          path="/admin/user/:id"
-          isAdmin={true}
-          component={UpdateUser}
-        />
-
-        <ProtectedRoute
-          exact
-          path="/admin/reviews"
-          isAdmin={true}
-          component={ProductReviews}
-        />
-
         <Route
           component={
             window.location.pathname === "/process/payment" ? null : NotFound
           }
-        /> */}
+        /> 
       </Switch>
-
       <Footer />
     </Router>
   );
 }
-
 export default App;
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,39 +1,18 @@
 import React, { Fragment, useEffect } from 'react';
-// import Cards from '../layout/Card/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import "./Home.css";
-// import bg1 from '../../images/i1.jpg';
 import c1 from '../../images/rb-c1.png'
-// import bg2 from '../../images/i2.jpg';
-// import bg3 from '../../images/i3.jpg';
-// import bgi1 from '../../images/bgi1.jpg'
-// import bgi2 from '../../images/bgi2.jpg'
 import bgi3 from '../../images/bgi3.jpg'
-import bgi4 from '../../images/bgi4.jpg'
-// import img1 from '../../images/img1.png';
-// import img2 from '../../images/img2.png';
-// import img3 from '../../images/img3.png';
-// import img4 from '../../images/img4.png';
-// import b1 from '../../images/b1.jpg';  
+import bgi4 from '../../images/bgi4.jpg' 
 import b2 from '../../images/rb-b3.png';
-// import f1 from '../../images/f1.jpg';
 import catsaree from "../../images/f2.jpg"
 import catcc from "../../images/f3.jpg"
 import catg from "../../images/img3.png"
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import whitebg from '../../images/whitebg.jpg';
-// import { FaShoppingCart } from 'react-icons/fa';
-// import { FaStar } from 'react-icons/fa';
-// import { CgMouse } from "react-icons/all";
 import ProductCard from "./ProductCard.js";
-// import MetaData from "../layout/MetaData";
 import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
-// import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
 import { NavLink } from 'react-router-dom';
-// import { Title } from 'chart.js';
 
 const Home = () => {
 
@@ -62,18 +41,8 @@ const Home = () => {
               src={c1}
               alt="First slide"
             />
-
             <Carousel.Caption>
-              {/* <div className='headline'>
-                <p className='tag-bit'>Womens collection</p>
-                <p className='tagline'>For colorfull steps in life</p>
-                <button className='btn btn-outline-dark btn-head'>SHOP NOW</button>
-              </div> */}
-              {/* <h1>title 1</h1> */}
-              
-
             </Carousel.Caption>
-
           </Carousel.Item>
           <Carousel.Item>
             <img
@@ -81,15 +50,7 @@ const Home = () => {
               src={bgi3}
               alt="Second slide"
             />
-
-            <Carousel.Caption>
-              {/* <div className='headline'>
-                <p>Highest Quility <br /> Honest prices</p>
-              </div> */}
-              {/* <h1>title 1</h1> */}
-              
-            </Carousel.Caption>
-
+            <Carousel.Caption></Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
@@ -97,15 +58,7 @@ const Home = () => {
               src={bgi4}
               alt="Third slide"
             />
-
-            <Carousel.Caption>
-              {/* <div className='headline'>
-                <p>
-                  Discover
-                </p>
-              </div> */}
-              {/* <h1>title 1</h1> */}
-            </Carousel.Caption>
+            <Carousel.Caption></Carousel.Caption>
           </Carousel.Item>
         </Carousel>
 
@@ -120,7 +73,7 @@ const Home = () => {
             <img src={catsaree}></img>
             <div className="info">
               <h3 className='cat-head'>Saree</h3>
-              <button className='b1'><NavLink className="see-more" to="/saree">See More</NavLink></button>
+              <button className='b1'><NavLink className="see-more" to="/products">See More</NavLink></button>
             </div>
           </div>
 
@@ -128,7 +81,7 @@ const Home = () => {
             <img src={catcc}></img>
             <div className="info">
               <h3 className='cat-head'>Chaniya Choli</h3>
-              <button className='b2'><NavLink className="see-more" to="/chaniya-choli">See More</NavLink></button>
+              <button className='b2'><NavLink className="see-more" to="/products">See More</NavLink></button>
             </div>
 
           </div>
@@ -137,7 +90,7 @@ const Home = () => {
             <img src={catg}></img>
             <div className="info">
               <h3 className='cat-head'>Gowns</h3>
-              <button className='b3'><NavLink className="see-more" to="/gowns">See More</NavLink></button>
+              <button className='b3'><NavLink className="see-more" to="/products">See More</NavLink></button>
             </div>
           </div>
 
@@ -159,11 +112,6 @@ const Home = () => {
             ))}
         </div>
       </section>
-
-
-
-
-
     </div>
   );
 }
